6f2daaf38fb407761b15fc280020ef2b9c8788b8
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:49:06 -0700

    Add files via upload

---
+++ Javaprogram3.java
@@ -0,0 +1,13 @@
+
+public class Javaprogram3 {
+	int 123ab;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int enum;
+string; 
+System.out.println(Javaprogram3.123ab);// error: can't start variable name with digits
+System.out.println(enum);//error: can't give varible name as reserved keyword
+System.out.println(name);//error: java is case sensitive
+	}
+
+}
