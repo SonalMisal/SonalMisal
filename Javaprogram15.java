b1d615a3e8c1f0b7baad3884ec18ebea1e786a3c
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:53:18 -0700

    Add files via upload

---
+++ Javaprogram15.java
@@ -0,0 +1,15 @@
+
+public class Javaprogram15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 char a='a';
+	        if (a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+	            System.out.println("Character "+a+" is vowel.");
+	        }
+	        else{
+	            System.out.println("Character "+a+" is consonant.");
+	        }
+	}
+
+}
