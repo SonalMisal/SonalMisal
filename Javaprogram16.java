a4f2637b0003da49a9b7bf04d6f0767788b4b0c0
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:53:44 -0700

    Add files via upload

---
+++ Javaprogram16.java
@@ -0,0 +1,12 @@
+
+public class Javaprogram16 {
+	//code for gst calculating
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  float GST;
+	        int basePrice=34900;
+	        GST=basePrice*(18.0f/100);
+	        System.out.println("You have to pay RS. "+GST+"GST amount on RS. 34900");
+	}
+
+}
