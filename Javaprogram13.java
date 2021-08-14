020aa61d28f99587cd7c1fe322a6c2e72780a3e7
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:52:33 -0700

    Add files via upload

---
+++ Javaprogram13.java
@@ -0,0 +1,16 @@
+
+public class Javaprogram13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int first=100,second=500;
+        int temp;
+        System.out.println("Before swapping: first= "+first+" second= "+second);
+        temp=first;
+        first=second;
+        second=temp;
+        System.out.println("After swapping: first= "+first+" second= "+second);
+
+	}
+
+}
