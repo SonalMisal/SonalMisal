5967f9ae22aeaab2ba7370532067ee940ba3ec38
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:51:45 -0700

    Add files via upload

---
+++ Javaprogram11.java
@@ -0,0 +1,12 @@
+
+public class Javaprogram11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int x='x';
+	        int X='X';
+	        System.out.println("ASCII of x "+ x);
+	        System.out.println("ASCII of X "+ X);
+	}
+
+}
