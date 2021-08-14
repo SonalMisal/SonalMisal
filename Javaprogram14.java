c5d1e165af2ca6e0e62a3d12be94912bfa184dbb
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:52:54 -0700

    Add files via upload

---
+++ Javaprogram14.java
@@ -0,0 +1,15 @@
+
+public class Javaprogram14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int num=17;
+	        if (num%2==0){
+	            System.out.println("Number is Even.");
+	        }
+	        else{
+	            System.out.println("Number is odd.");
+	        }
+	}
+
+}
