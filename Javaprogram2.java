57393895ad756431223369514b37a233b9375ed7
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:48:37 -0700

    Add files via upload

---
+++ Javaprogram2.java
@@ -0,0 +1,9 @@
+
+public class Javaprogram2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("\" can we print '\\' with System.out.println() statement? \"");
+	}
+
+}
