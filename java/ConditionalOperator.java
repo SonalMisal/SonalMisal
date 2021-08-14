99b60dfdd9183adec7dc5dec83b2cf9def2c778a
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 03:11:53 -0700

    Delete folder

--- java/ConditionalOperator.java
+++
@@ -1,9 +0,0 @@
-
-class ConditionalOperator
-{
-public static void main(String args[])
-{
-System.out.pritln(str);
-String str=(n%2==0)? System.out.println("Number is even "):System.out.println("Number is odd");
-}
-}
