c4c9161985d9681df187323fba61f9a82bb437ef
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:49:29 -0700

    Add files via upload

---
+++ Javaprogram4.java
@@ -0,0 +1,16 @@
+
+public class Javaprogram4 {
+	 static String name;
+	   static int id ;
+	   double d;
+	   static float f;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				System.out.println(name); //null
+		        System.out.println(id); //0
+		        System.out.println(d); //Error cannot access nonstatic Field
+		        System.out.println(f); //0.0
+	}
+
+}
