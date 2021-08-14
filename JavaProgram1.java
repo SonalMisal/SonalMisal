3aa3dcf53db541ef934484aae838c1e78f6778d3
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:48:08 -0700

    Add files via upload

---
+++ JavaProgram1.java
@@ -0,0 +1,14 @@
+ 
+public class JavaProgram1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Sonal"+"sonal@gmail.com"+"p01301"+"7028821018");
+		System.out.println("Ramesh"+"ramesh@gmail.com"+"p01302"+"9665254123");
+		System.out.println("Rohit"+"rohit@gmail.com"+"p01303"+"9766596634");
+		System.out.println("Komal"+"komal@gmail.com"+"p01304"+"7018963479");
+		System.out.println("Sanika"+"sanika@gmail.com"+"p01305"+"1058369741");
+
+	}
+
+}
