6ec686b7e895008b91a4efbdb48713f01a21ae57
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:52:06 -0700

    Add files via upload

---
+++ Javaprogram12.java
@@ -0,0 +1,12 @@
+
+public class Javaprogram12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+        int b=12;
+        System.out.println("Quotient of a/b is "+a/b);//quotient
+        System.out.println("Remainder of a%b is "+a%b);//remainder
+	}
+
+}
