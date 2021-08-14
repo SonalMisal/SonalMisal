dd2376f5d3b52959547381e12ee2cc80388ac86f
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-14 08:49:55 -0700

    Add files via upload

---
+++ Javaproject5.java
@@ -0,0 +1,24 @@
+
+public class Javaproject5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Integer Max & Min value
+		 System.out.println("Integer Minvalue: "+Integer.MIN_VALUE+"\t Integer MaxValue:"+Integer.MAX_VALUE+ "\t Integer size "+ Integer.SIZE +" bits");
+		 // Short Max & Min value
+         System.out.println("Short Minvalue: "+Short.MIN_VALUE+"\t Short MaxValue:"+Short.MAX_VALUE+ "\t Short size "+ Short.SIZE +" bits");
+         // Float Max &Min value
+         System.out.println("Float Minvalue: "+Float.MIN_VALUE+"\t Float MaxValue:"+Float.MAX_VALUE+ "\t Float size "+ Float.SIZE +" bits");
+         // Double Max & Min value
+         System.out.println("Double Minvalue: "+Double.MIN_VALUE+"\t Double MaxValue:"+Double.MAX_VALUE+ "\t Double size "+ Double.SIZE +" bits");
+         // Character Max & Min value 
+         System.out.println("Character Minvalue: "+Character.MIN_VALUE+"\t Character MaxValue:"+Character.MAX_VALUE+ "\t Character size "+ Character.SIZE +" bits");
+         // Long Max & Min value
+         System.out.println("Long Minvalue: "+Long.MIN_VALUE+"\t Long  MaxValue:"+Long.MAX_VALUE+ "\t Long size "+ Long.SIZE +" bits");
+         // Byte Max & Min value
+         System.out.println("Byte Minvalue: "+Byte.MIN_VALUE+"\t Byte MaxValue:"+Byte.MAX_VALUE+ "\t Byte size "+ Byte.SIZE +" bits");
+
+	}
+	}
+
+
