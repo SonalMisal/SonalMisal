b365805cb67601df5db1f148366c6a2085adb8a2
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-15 21:47:40 -0700

    Add files via upload

---
+++ Javaprogram20.java
@@ -0,0 +1,19 @@
+1. if-then statement.
+2. swith statement.
+3. do-while statement.
+
+ 4.public void infiniteLoopUsingFor()
+ {
+	for(;;)
+	{
+		// statements
+	}
+ }
+
+5.public void infiniteLoopUsingWhile()
+{
+	while(true)
+	{
+		// statements
+	}
+}
