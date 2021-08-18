f4aa9f84c0882e1690d6e6fed9c00d4cf185e407
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-17 21:59:02 -0700

    Add files via upload

---
+++ String.java
@@ -0,0 +1,13 @@
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+        String x="27-11-2003";
+        String d=x.split("-")[0];
+        String m=x.split("-")[1];
+        String y=x.split("-")[2];
+
+        System.out.println("Year : "+y);
+	}
+}
