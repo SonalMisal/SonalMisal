9c7b10dc6b923c4bc9bafbdfd3b61cceb344d9a7
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-17 21:57:54 -0700

    Add files via upload

---
+++ Assignment4.java
@@ -0,0 +1,16 @@
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        int per=30; //percentage = 30%
+		        int numerator=per;
+		        int denominator=100;
+		        System.out.println(" Fraction of "+ per+" % is: "+numerator+"/"+denominator);
+		        numerator=per/10;
+		        denominator=100/10;
+		        System.out.println(" Fraction of "+ per+" % is: "+numerator+"/"+denominator);
+
+	}
+
+}
