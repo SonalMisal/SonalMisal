8f8daf65300e241f3534bb060693a2f2dcc8f95e
Sonal R. Misal <88672000+SonalMisal@users.noreply.github.com>
2021-08-17 21:58:38 -0700

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+
+// Class ReverseString
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+String input = "JavaSkills";
+StringBuilder input1 =new StringBuilder();
+// append a string into StringBuilder input1
+input1.append(input);
+// reverse StringBuilder input1
+input1.reverse();
+// print reversed String
+System.out.println(input1);
+	}
+
+}
